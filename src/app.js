3b7e6deabc99b3daaf03b3587467416e4d090992
Matt Jackson <matthew.jackson@publicismedia.com>
2020-01-21 12:15:26 -0500

    Update Konami code alert

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 function app() {
     function addKonamiListener() {
         const konamiCode = [
-            'ArrowUp',
-            'ArrowUp',
-            'ArrowDown',
-            'ArrowDown',
-            'ArrowLeft',
-            'ArrowRight',
-            'ArrowLeft',
-            'ArrowRight',
-            'b',
+            // 'ArrowUp',
+            // 'ArrowUp',
+            // 'ArrowDown',
+            // 'ArrowDown',
+            // 'ArrowLeft',
+            // 'ArrowRight',
+            // 'ArrowLeft',
+            // 'ArrowRight',
+            // 'b',
             'a',
         ];
         let konamiCodePosition = 0;
@@ -19,7 +19,7 @@ function app() {
             if (e.key === requiredKey) {
                 konamiCodePosition++;
                 if (konamiCodePosition === konamiCode.length) {
-                    alert('leet hekker!'); // TODO
+                    alert('You won my email address! \n matjack9@gmail.com');
                     konamiCodePosition = 0;
                 }
             } else {
